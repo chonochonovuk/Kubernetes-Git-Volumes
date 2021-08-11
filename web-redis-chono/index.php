f84a0274f1e8472e61d3b94d935949a8a5c1aa1d
Chono Chonov <chonochonovuk@gmail.com>
2021-08-11 18:24:15 +0100

    Redis Web Page

---
+++ web-redis-chono/index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
+<body>  
+
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = "";
+$name = $email = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed";
+    }
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+    }
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<h2>PHP Form Validation Example</h2>
+<p><span class="error">* required field</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name" value="<?php echo $name;?>">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require 'Predis/Autoloader.php';
+
+Predis\Autoloader::register();
+
+  $host = 'redis-host';
+
+  $client = new Predis\Client([
+      'scheme' => 'tcp',
+      'host'   => $host,
+      'port'   => 6379,
+    ]);
+  
+  if(exist("message"){
+   $value = $client->get("message");
+   $value += $name.' '.$email;
+   $client->set("message", $value);
+   print($value." ");
+  }else{
+    $client->set("message", $name.' '.$email);
+  }
+  
+#echo "<h2>Your Input:</h2>";
+#echo $name;
+#echo "<br>";
+#echo $email;
+#echo "<br>";
+
+?>
+
+</body>
+</html>
+
+
+<html>
+<head>
+</head>
+<body>
+Hello! I have been visited: 
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require 'Predis/Autoloader.php';
+
+Predis\Autoloader::register();
+
+  $host = 'redis-master';
+
+  $client = new Predis\Client([
+      'scheme' => 'tcp',
+      'host'   => $host,
+      'port'   => 6379,
+    ]);
+
+  $value = $client->incr("visited");
+  
+  print($value." ");
+?>
+time(s) so far.
+<br /><br /><br />
+<i>Powered by <b>Redis</b></i>
+</body>
+</html>
