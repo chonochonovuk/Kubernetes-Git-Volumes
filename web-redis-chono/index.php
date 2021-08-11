08457ec05af4170b8f230c22c85a53af26f1b014
Chono Chonov <chonochonovuk@gmail.com>
2021-08-11 19:15:19 +0100

    Redis Web Page

--- web-redis-chono/index.php
+++ web-redis-chono/index.php
@@ -72,8 +72,10 @@ Predis\Autoloader::register();
       'port'   => 6379,
     ]);
   
+   $value = $client->get("account");
+   if(empty($value)){
    $client->set("account", $name.' '.$email);
-  
+   }
 #echo "<h2>Your Input:</h2>";
 #echo $name;
 #echo "<br>";
