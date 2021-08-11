ce4012db9c044bf96a55f2ddf17f9a96ccb68e2d
Chono Chonov <chonochonovuk@gmail.com>
2021-08-11 18:32:44 +0100

    Redis Web Page

--- web-redis-chono/index.php
+++ web-redis-chono/index.php
@@ -72,12 +72,12 @@ Predis\Autoloader::register();
       'port'   => 6379,
     ]);
   
-  if(exist("message"){
+  if (exist("message") {
    $value = $client->get("message");
    $value += $name.' '.$email;
    $client->set("message", $value);
    print($value." ");
-  }else{
+  } else {
     $client->set("message", $name.' '.$email);
   }
   
@@ -89,38 +89,5 @@ Predis\Autoloader::register();
 
 ?>
 
-</body>
-</html>
-
-
-<html>
-<head>
-</head>
-<body>
-Hello! I have been visited: 
-<?php
-
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-require 'Predis/Autoloader.php';
-
-Predis\Autoloader::register();
-
-  $host = 'redis-master';
-
-  $client = new Predis\Client([
-      'scheme' => 'tcp',
-      'host'   => $host,
-      'port'   => 6379,
-    ]);
-
-  $value = $client->incr("visited");
-  
-  print($value." ");
-?>
-time(s) so far.
-<br /><br /><br />
-<i>Powered by <b>Redis</b></i>
 </body>
 </html>
